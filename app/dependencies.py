5290153d1e84a15261f2dbe6ede96b228f0d7abf
Renato Esposito <renato.esposito1999@outlook.com>
2025-10-28 15:01:46 +0100

    changed gitignore form python to java

--- app/dependencies.py
+++
@@ -1,8 +0,0 @@
-from app.models.db import Database
-from app.services.admin_service import AdminService
-
-db = Database()
-admin_service = AdminService(db)
-
-def get_admin_service():
-    return admin_service
